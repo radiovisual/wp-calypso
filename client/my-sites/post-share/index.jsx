fcd4b5900b13348fb6f862e0843b3a276f73f56a
Takashi Irie <takashi@automattic.com>
2017-03-21 19:24:01 +0000

    Sharing: Update style for Republicize

--- client/my-sites/post-share/index.jsx
+++ client/my-sites/post-share/index.jsx
@@ -356,7 +356,7 @@ class PostShare extends Component {
 				<div className="post-share__main">
 					<div className="post-share__form">
 						{ this.renderMessage() }
-						{ this.renderShareButton() }
+						{ this.renderSharingButtons() }
 					</div>
 
 					{ this.renderConnectionsSection() }
